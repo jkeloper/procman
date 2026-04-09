bd7a1c9b24a70b8bc36f598402a0906970fde135
procman-worker <procman@local>
2026-04-09 13:21:03 +0900

    fix: add tauri-plugin-fs for log export

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -57,6 +57,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_fs::init())
         .manage(app_state)
         .manage(runtime_store)
         .setup(move |app| {
